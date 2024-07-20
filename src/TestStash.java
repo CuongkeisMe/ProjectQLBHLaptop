199f97db74b0cbd4104f12b5c7b5d43add2b886e
CuongkeisMe <nqc23082005@gmail.com>
2024-07-21 01:21:29 +0700

    cuong

---
+++ src/TestStash.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+
+public class TestStash {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        
+    }
+}
